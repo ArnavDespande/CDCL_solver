6f51652d0ef5330e675e52539c389806fb2d0c98
ArnavD <89904979+ArnavDespande@users.noreply.github.com>
2024-10-22 14:52:44 +0100

    minor return value tweaks

--- ad_CDCL_solver.py
+++ ad_CDCL_solver.py
@@ -8,12 +8,10 @@ vars = details[1]
 
 clauses = filereading.get_clauses(cnf_file)
 
-print("Found clauses: ", clauses)
+print("Found clauses:", clauses)
 print()
 
 preoptimized_clauses = preoptimizer.pre_optimizer(clauses, vars)
-print("Pre-optimized clauses: ", preoptimized_clauses)
-print()
 
 pre_CDCL_clauses = preoptimizer.recursive_contradiction_optimizer(preoptimized_clauses)
-print("Final pre-CDCL result", pre_CDCL_clauses)
+print("Final pre-CDCL result:", pre_CDCL_clauses)
--- preoptimizer.py
+++ preoptimizer.py
@@ -7,7 +7,7 @@ def recursive_contradiction_optimizer(clauses):
     print()
 
     if (len(singulars) > 0):
-        if (contradiction_check(clauses, singulars) == "UNSATISFIABLE"):
+        if (contradiction_check(singulars) == "UNSATISFIABLE"):
             return "UNSATISFIABLE"
         clauses = singularity_eliminator(clauses, int(singulars[0][0]))
         return recursive_contradiction_optimizer(clauses)
@@ -25,7 +25,6 @@ def pre_optimizer(clauses, vars):
 def singularity_eliminator(clauses, singular):
 
     removables = []
-    removable_singlevalues = []
 
     for clause in clauses:
         if (singular in clause):
@@ -34,12 +33,12 @@ def singularity_eliminator(clauses, singular):
             clause.remove(-singular)
 
     clauses = [i for i in clauses if i not in removables]
-    print("With singularity literals addressed:", clauses)
+    print("With singular clause [",singular,"] addressed:", clauses)
     print()
 
     return clauses
 
-def contradiction_check(clauses, singulars):
+def contradiction_check(singulars):
 
     singulars = [int(item[0]) for item in singulars]
 
@@ -47,14 +46,12 @@ def contradiction_check(clauses, singulars):
         if (-singular in singulars):
             return "UNSATISFIABLE"
 
-    return "UNKNOWN"
-
 def pure_literal_eliminator(clauses, vars):
 
     pure_clauses = []
 
     pures = clause_analysis.get_pure_literals(clauses, vars)
-    print("Pure literals: ", pures)
+    print("Pure literals:", pures)
     print()
 
     for pure in pures:
@@ -63,15 +60,17 @@ def pure_literal_eliminator(clauses, vars):
                 pure_clauses.append(clause)
 
     clauses = [i for i in clauses if i not in pure_clauses]
+    print("With pure-literals addressed:", clauses)
+    print()
     return clauses
 
 def tautological_eliminator(clauses):
 
     tautologies = clause_analysis.get_tautological_clauses(clauses)
-    print("Tautologies found: ", tautologies)
+    print("Tautologies found:", tautologies)
     print()
     clauses = [i for i in clauses if i not in tautologies]
-    print("With tautologies removed: ", clauses)
+    print("With tautologies removed:", clauses)
     print()
     
     return clauses
@@ -87,4 +86,6 @@ def singularity_compressor(clauses):
                 subclause.append(literal)
         clauses1.append(subclause)
 
+    print("Compressing all repeated values:", clauses1)
+    print()
     return clauses1
