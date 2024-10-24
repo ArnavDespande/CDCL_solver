3085410adaba5f4aa63161b11b7801001b148fbf
ArnavD <89904979+ArnavDespande@users.noreply.github.com>
2024-10-24 03:26:47 +0100

    rewrote algorithms to be less destructive

--- ad_CDCL_solver.py
+++ ad_CDCL_solver.py
@@ -1,23 +1,32 @@
-import file_reading
-import preoptimizer
-import CDCL_algorithm
+from utilities.CDCL_heuristics import random_value
+from utilities.file_reading import *
+from solvers.preoptimizer import *
+from solvers.CDCL_algorithm import *
+from custom_types.Clause_table import *
 
 
-cnf_file = file_reading.read_file("C:/Users/arnav/Desktop/cnf.cnf")
+cnf_file = read_file("C:/Users/arnav/Desktop/cnf.cnf")
 
-details = file_reading.get_base_details(cnf_file)
+details = get_base_details(cnf_file)
 vars = details[0]
 
-clauses = file_reading.get_clauses(cnf_file)
+clauses = get_clauses(cnf_file)
 print("Found clauses:", clauses)
 print()
 
-preoptimized_clauses = preoptimizer.pre_optimizer(clauses, vars)
-pre_CDCL_clauses = preoptimizer.recursive_contradiction_optimizer(preoptimized_clauses)
+preoptimized_clauses = pre_optimizer(clauses, vars)
+pre_CDCL_clauses = recursive_contradiction_optimizer(preoptimized_clauses)
 
 print("Optimized clauses: ", pre_CDCL_clauses)
 print()
 
-if (pre_CDCL_clauses != 'UNSATISFIABLE' and pre_CDCL_clauses != 'SATISFIABLE'):
-    robotic_result = CDCL_algorithm.solver(pre_CDCL_clauses, vars)
-    print("Result:", robotic_result)
+if (pre_CDCL_clauses != "UNSATISFIABLE"):
+    table_map = setup_table(pre_CDCL_clauses)
+    while (table_map != False and table_map != True and not find_false_contradiction(table_map)):
+        literal_selected = random_value(table_map, vars)
+        table_map = solver_till_decision(table_map, literal_selected)
+        if (table_map != False and table_map != True):
+            print(table_map.print_table())
+        else:
+            print(table_map)
+        print()
