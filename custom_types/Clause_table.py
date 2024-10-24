92e4e73599da9eb61440ee4802e6f25e49aee1b3
ArnavD <89904979+ArnavDespande@users.noreply.github.com>
2024-10-24 03:31:40 +0100

    made custom datatypes

---
+++ custom_types/Clause_table.py
@@ -0,0 +1,52 @@
+class Value:
+    def __init__ (self, tag, value=None, assigned_status=False):
+        self.tag = tag
+        self.value = value
+        self.assigned_status = assigned_status
+
+class Clause:
+    def __init__ (self, vars):
+        self.variables = vars
+
+    def get_clause_value(self):
+        unassigned_located = False
+        for variable in self.variables:
+            if (variable.assigned_status):
+                if (variable.value):
+                    return True
+            else:
+                unassigned_located = True
+        if (unassigned_located):
+            return None
+        return False
+
+    def print_clause(self):
+        for var in self.variables:
+            print(var.tag, " ", var.value, " ", end="")
+            print()
+
+class Clause_Table:
+    def __init__ (self, clauses):
+        self.clauses = []
+
+    def add_clause(self, clause):
+        self.clauses.append(clause)
+
+    def print_table(self):
+        for clause in self.clauses:
+            for var in clause.variables:
+                print(var.tag , " ", var.value, " ", end="")
+            print()
+
+def setup_table(clauses):
+    setup = Clause_Table([])
+
+    for clause in clauses:
+        valued_clause = []
+        for var in clause:
+            value = Value(var)
+            valued_clause.append(value)
+        adding_clause = Clause(valued_clause)
+        setup.add_clause(adding_clause)
+
+    return setup
--- custom_types/Trail.py
+++ custom_types/Trail.py
@@ -1,1 +1,14 @@
+class Trail:
+    def __init__ (self, value, reason, predecessor=None):
+        self.value = value
+        self.reason = reason
+        self.predecessor = predecessor
 
+
+def get_decision_level(trail_node):
+    decision_level = 0
+    while trail_node is not None:
+        if (trail_node.reason == "DECISION"):
+            decision_level += 1
+        trail_node = trail_node.predecessor
+    return decision_level
